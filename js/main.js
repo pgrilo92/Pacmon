3793bd1eac63f7efe9536e90cf56418013f7bde4
Joaquim <pgrilo92@hotmail.com>
2019-11-30 15:22:14 -0500

    adding temporary movement of pacmon

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-    
+    let i = 2
+    $('body').keypress((evt)=>{
+        let key = evt.which
+        if(key == 39){
+            $('button').trigger('click')
+            return false
+        }
+    })
+    $('button').on('click', ()=>{
+        if(i <=5 ){
+            let $moveRight = $('#pacmon').detach().appendTo(`#${i}`)
+            i++
+        }
+    })
 })

01f695292cf1b4e9525716e83ecc67f824801a0e
Joaquim <pgrilo92@hotmail.com>
2019-11-30 22:29:10 -0500

    add continuous movement to pacmon

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(function(){
+/*----- Variables-----*/ 
     let collumnNumber = 8
     let rowNumber = 18
+    let changeDirection = false
+    let timeID
+    let pacmonSpeedTimer = 600
+/*---- Classes ---*/
+/*----- Moving functions for Pacmon -----*/
     $('body').keypress((evt)=>{
         let key = evt.which
         if(key == 39){
@@ -9,27 +15,56 @@ $(function(){
         }
     })
     $('#up-btn').on('click', ()=>{
-        if(rowNumber > 1){
-            rowNumber--
-            let $moveRight = $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`)
+        clearInterval(timeID)
+        function moveUpFunc(){
+            if(rowNumber > 1 ){
+                rowNumber--
+                let $moveRight= $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`).show('slow')
+            } else if(rowNumber <= 1 || changeDirection === true) {
+                clearInterval(timeID)
+                changeDirection = false
+            }
         }
+        timeID = setInterval(moveUpFunc, pacmonSpeedTimer)
     })
-    $('#right-btn').on('click', ()=>{
-        if(collumnNumber < 16){
-            collumnNumber++
-            let $moveRight = $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`)
+    $('#right-btn').on('click', ()=>  {
+        clearInterval(timeID)
+        function moveRightFunc(){
+            if(collumnNumber < 17 ){
+                collumnNumber++
+                let $moveRight= $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`).show('slow')
+            } else if(collumnNumber >= 20 || changeDirection === true) {
+                clearInterval(timeID)
+                changeDirection = false
+            }
         }
+        timeID = setInterval(moveRightFunc, pacmonSpeedTimer)
     })
     $('#down-btn').on('click', ()=>{
-        if(rowNumber < 20){
-            rowNumber++
-            let $moveRight = $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`)
+        clearInterval(timeID)
+        function moveDownFunc (){
+            if(rowNumber < 20 ){
+                rowNumber++
+                let $moveRight= $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`).show('slow')
+            } else if(rowNumber >= 20 || changeDirection === true) {
+                clearInterval(timeID)
+                changeDirection = false
+            }
         }
+        timeID = setInterval(moveDownFunc, pacmonSpeedTimer)
     })
     $('#left-btn').on('click', ()=>{
-        if(collumnNumber > 1){
-            collumnNumber--
-            let $moveRight = $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`)
+        clearInterval(timeID)
+        function moveLeftFunc(){
+            if(collumnNumber > 1 ){
+                collumnNumber--
+                let $moveRight= $('#pacmon').detach().appendTo(`#r${rowNumber}c${collumnNumber}`).show('slow')
+            } else if(collumnNumber <= 1 || changeDirection === true) {
+                clearInterval(timeID)
+                changeDirection = false
+            }
         }
+        timeID = setInterval(moveLeftFunc, pacmonSpeedTimer)
     })
+/
 })
